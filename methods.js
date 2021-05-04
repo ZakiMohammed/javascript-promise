d5cbfa5d4ee0281c0b67061f90f1741d7f1ffd6d
Zaki Shaikh <zaki-shaikh@outlook.com>
2021-05-05 00:20:13 +0530

    Promise methods updated

--- methods.js
+++ methods.js
@@ -1,26 +1,26 @@
-const promise1 = new Promise((resolve, reject) => setTimeout(() => resolve('resolve 1'), 3000));
-const promise2 = new Promise((resolve, reject) => setTimeout(() => resolve('resolve 2'), 500));
-const promise3 = new Promise((resolve, reject) => setTimeout(() => resolve('resolve 3'), 2000));
-const promise4 = new Promise((resolve, reject) => setTimeout(() => resolve('resolve 4'), 1500));
-const promise5 = new Promise((resolve, reject) => setTimeout(() => resolve('resolve 5'), 2500));
+const p1 = new Promise((resolve, reject) => setTimeout(() => reject('reject 1'), 3000));
+const p2 = new Promise((resolve, reject) => setTimeout(() => reject('reject 2'), 500));
+const p3 = new Promise((resolve, reject) => setTimeout(() => reject('reject 3'), 2000));
+
+const promises = [p1, p2, p3];
 
 // wait until all are resolved, go to catch if any of them rejected
-Promise.all([promise1, promise2, promise3, promise4, promise5])
-    .then(promises => {
-        promises.forEach(result => console.log(result));
-    })
+Promise.all(promises)
+    .then(result => result.forEach(result => console.log(result)))
     .catch(error => console.log(error));
+    
 
-// wait until all are resolved or rejected, show status of each in this format: { status: 'fulfilled', value: 'resolve 1' }
-Promise.allSettled([promise1, promise2, promise3, promise4, promise5])
-    .then(promises => {
-        promises.forEach(result => console.log(result));
-    })
-    .catch(error => console.log(error));
+// // wait until all are resolved or rejected, show status of each in this format: { status: 'fulfilled', value: 'resolve 1' }
+// Promise.allSettled(promises)
+//     .then(result => result.forEach(result => console.log('allSettled', result)))
+//     .catch(error => console.log('allSettled', error));
 
-// wait until any of them either resolved or rejected
-Promise.race([promise1, promise2, promise3, promise4, promise5])
-    .then(result => {
-        console.log(result)
-    })
-    .catch(error => console.log(error));
+// // wait until any of them either resolved or rejected
+// Promise.race(promises)
+//     .then(result => console.log('race', result))
+//     .catch(error => console.log('race', error));
+
+// // wait until any of them resolved, go to catch if all of them rejected (opposite of all)
+// Promise.any(promises)
+//     .then(result => console.log('any', result))
+//     .catch(error => console.log('any', error));
